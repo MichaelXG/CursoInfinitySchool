e263c082f7e846dd5abbe1a38350551db9e41b85
Michael XG <michaelxg@gmail.com>
2024-02-19 17:07:22 -0300

    Prova_01 - LP-A01

---
+++ Lógica_de_programação/Aula_01/LP-A01.py
@@ -0,0 +1,26 @@
+# Crie um exemplo de cada uma das principais tipagens de variáveis.
+# String
+nome = "Michael"
+print(f'Nome : {nome} type : {type(nome)})')
+
+ #Inteiro
+numero = 1985
+print(f'Número : {numero} type : {type(numero)})')
+ 
+# Float
+Valor = 150.35
+print(f'Valor : {Valor} type : {type(Valor)})')
+ 
+# Boolean
+semExiste = True
+print(f'Existe : {semExiste} type : {type(semExiste)})')
+
+#--------------------------------------------------
+# Crie uma solicitação de dados ao usuário.
+
+solicitacao = input(f'Qual o seu nome ?)')
+
+print(f'Resposta : {solicitacao}')
+
+#--------------------------------------------------
+
