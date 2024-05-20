65ce49e6cde6e12048ed43a27f32902452d7ea2d
Michael XG <michaelxg@gmail.com>
2024-05-20 11:19:14 -0300

    Provas
    Provas
    
    Signed-off-by: Michael XG <michaelxg@gmail.com>

---
+++ Provas/PY-A07.py
@@ -0,0 +1,19 @@
+print("""
+    [PY-A07] Você foi contratado(a) para desenvolver um programa que gerencie um dicionário de alunos onde 
+    a chave deste dicionário é o número de matrícula dos próprios alunos. 
+    O programa deve permitir adicionar, remover, atualizar e listar os alunos.
+
+    Para isso, você deve implementar um módulo que contém as seguintes funções:
+
+    1 . AdicionarAluno(): Solicita ao usuário que digite o nome e o número de matrícula de um aluno 
+    e adicione-o ao dicionário de alunos.
+
+    2 . RemoverAluno(): Solicita ao usuário que digite o número de matrícula de um aluno e remove-o do 
+    dicionário de alunos.
+
+    3. AtualizarAluno(): Solicita ao usuário que digite o número de matrícula de um aluno e atualize 
+    o nome desse aluno no dicionário .
+
+    4. VerAlunos(): Lista todos os alunos cadastrados, exibindo o número de matrícula e o nome de cada um.
+
+""")
